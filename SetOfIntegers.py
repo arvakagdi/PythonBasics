dd0efddd3da0dd9954a54e7ea75f183584e460c1
Arva Kagdi <arvamkagdi@gmail.com>
2020-02-10 13:59:49 -0500

    Class with add, remove and member functions for a list

---
+++ SetOfIntegers.py
@@ -0,0 +1,61 @@
+'''
+Creating a collection of Integers
+A particular integer appears only once in a set
+
+List - data representations
+
+Interface:
+insert()
+member()
+remove()
+'''
+
+class intSet (object):
+    ''' intSet  is a set of int. Value is represented by list of ints, self.vals. Each int can occur only once'''
+    
+    def __init__ (self):
+        ''' Creating an empty list of ints'''
+        self.vals = []
+        
+    def insert(self,num):
+        '''Assumes e as int and inserts it into self'''
+        if  not num in self.vals:
+            self.vals.append(num)
+    
+    def member(self,num):
+        '''Returns True if num in list else false'''
+        return num in self.vals
+    
+    def delete(self, num):
+        '''Deletes num from list if present else raises Valuerror'''
+        try:
+            self.vals.remove(num)
+        except:
+            raise ValueError(str(num) + 'Not in the list')
+    
+    def __str__ (self):
+        '''Returns string representation'''
+        self.vals.sort()
+        result = ''
+        
+        for num in self.vals:
+            result = result + str(num) + ','
+            
+        return '{' + result[:-1] + '}'
+    
+    
+    
+    
+newstr = intSet()
+    
+newstr.insert(1)
+newstr.insert(2)
+print(newstr)
+    
+newstr.insert(5)
+newstr.insert(10)
+print(newstr)    
+newstr.delete(1)
+print(newstr)
+print("5: " ,newstr.member(5))                     
+print("15: " ,newstr.member(15))                     
